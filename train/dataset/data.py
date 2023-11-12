47253a997e0de332b07ba416d347654925e7ddf8
Ramazuki <ramazukigamer@gmail.com>
2023-11-12 16:56:20 +0300

    Для обработки изображений для составления датасета

---
+++ train/dataset/data.py
@@ -0,0 +1,27 @@
+import cv2
+from PIL import Image
+from facenet_pytorch import MTCNN, InceptionResnetV1
+import os
+
+print('Введи название папки')
+folder = 'input'
+mtcnn = MTCNN(image_size=48, margin=0)
+resnet = InceptionResnetV1(pretrained='vggface2').eval()
+
+#читаем файлы в папке
+pictures = os.listdir(folder)
+#обрезаем
+for p in pictures:
+    way = folder + '/' + p
+    img = Image.open(way)
+    save = 'cropped/' + p
+    img_cropped = mtcnn(img, save_path=save)
+
+pictures = os.listdir('cropped')
+for p in pictures:
+    way = 'cropped/' + p
+    save = 'output/' + p
+    im_gray = cv2.imread(way, cv2.IMREAD_GRAYSCALE)
+    cv2.imwrite(save, im_gray)
+
+print('Готово')
